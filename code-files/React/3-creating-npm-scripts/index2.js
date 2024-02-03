15bc37093e7251da0a49297bd3b251e63f2fc2bd
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-03 16:29:59 +0530

    Changes to be committed: 	modified:   code-files/React/3-creating-npm-scripts/index.html 	modified:   code-files/React/3-creating-npm-scripts/index.js 	new file:   code-files/React/3-creating-npm-scripts/index2.js 	modified:   code-files/React/react-project-template/index.html

--- code-files/React/3-creating-npm-scripts/index2.js
+++ code-files/React/3-creating-npm-scripts/index2.js
@@ -1,11 +1,6 @@
 import React from '../../../node_modules/react' // importing 'React' module from node_modules/react
 import ReactDOM from '../../../node_modules/react-dom/client' // importing 'React' module from node_modules/react
 
-let elemBody = React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Child 1'),
-    React.createElement('div', {}, [
-        'Sibling of Child 1',
-        React.createElement('h2', {}, 'child of sibling of child 1'
-        )])]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(elemBody);
+console.log('test');
