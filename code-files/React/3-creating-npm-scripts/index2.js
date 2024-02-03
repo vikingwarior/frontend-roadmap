0514317269c2de6a987529dd043635f1ced09071
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-04 00:56:07 +0530

    Basics of JSX

--- code-files/React/3-creating-npm-scripts/index2.js
+++ code-files/React/3-creating-npm-scripts/index2.js
@@ -1,6 +1,26 @@
-import React from '../../../node_modules/react' // importing 'React' module from node_modules/react
-import ReactDOM from '../../../node_modules/react-dom/client' // importing 'React' module from node_modules/react
+import React from 'react' // importing 'React' module from node_modules/react
+import ReactDOM from 'react-dom/client' // importing 'ReactDOM' module from node_modules/react
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(elemBody);
+
+/**
+ * Equivalent of:
+ * React.createElement('h1', {}, 'React element using JSX🚀');
+ * 
+ * JSX syntax is parsed and converted to React.createElement() using Babel.
+ * Babel => JS complier
+ * 
+ * JSX => React.createElement(using Babel) => HTML_Element(when root.render is triggered)
+ *->Attributes inside JSX are written using camelCase
+ */
+let newJsxElem = (
+    <>
+    <input type='button' value='btn using jsx' />
+        <label>
+            <input type='radio' value='radio1'/>
+            Radio using JSX
+        </label>
+    </>
+);
+root.render(newJsxElem);
 console.log('test');
