809dcd26384c06037c030b0ed7b22bd880a6fba9
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-05 16:58:32 +0530

    Basics of React Functional Components

--- code-files/React/3-creating-npm-scripts/index2.js
+++ code-files/React/3-creating-npm-scripts/index2.js
@@ -22,5 +22,42 @@ let newJsxElem = (
         </label>
     </>
 );
-root.render(newJsxElem);
-console.log('test');
+
+// root.render(newJsxElem);
+
+/**
+ * Functional React Component ->
+ * A Functional React Component is a function that returns either JSX or React.createElement()
+ * 
+ * Rule ->
+ * It should be a function and always begin  with an Uppercase letter.
+ * 
+ * If we need to use a react component, it should be used a jsx element
+ * 
+ * For example
+ */
+
+const HeaderComponent = () => (
+    <div>
+        <h1>This is a React Functional Header Component</h1>
+    </div>
+);
+
+// root.render(<HeaderComponent/>);
+
+/**
+ * Component Composition -> This when one or more components are being used inside a component that is component composition
+ */
+
+const BodyComponent = () => (
+    <div>
+        <HeaderComponent />
+        <div className='body'>
+            <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+            This is a React Functional Body Component
+            </a>
+        </div>
+    </div>
+);
+
+root.render(<BodyComponent />);
