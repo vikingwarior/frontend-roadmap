7264abfc54c8f1ddc2dfeb32d51be03f7b4b1bc1
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-11 01:46:38 +0530

    Created header component

--- code-files/React/4-first-react-project/App.js
+++ code-files/React/4-first-react-project/App.js
@@ -1,36 +1,22 @@
 import React from 'react' // importing 'React' module from node_modules/react
-import ReactDOM from 'react-dom' // importing 'ReactDOM' module from node_modules/react
+import ReactDOM from 'react-dom/client' // importing 'ReactDOM' module from node_modules/react
 
-/**
- * App Component
- *      -Header
- *       --Logo
- *       --Location
- *       --cart
- *       --account
- *      
- *      -Body
- *       --Search
- *           ---search button
- *           ---search button
- *           ---filter
- *               ---price low to high
- *               ---price high to low
- *               ---ratings high to low
- *               ---distance nearest to farthest
- *               ---Cuisines offered
- *                   -----cuisines list
- *       
- *      --Restruants      
- *          ---Restrurant Card
- *           ----Image
- *           ----Rating
- *           ----Distance
- *           ----Average Price
- *           ----Cuisines offered
- *           
- *      -Footer
- *       --Contact
- *       --Help
- *       --About
- */
+const Header = () => (
+    <div className="header">
+        <img
+            src='https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png'
+            className='logo' />
+        <ul className='nav-options'>
+            <li>Home</li>
+            <li>Cart</li>
+            <li>Account</li>
+        </ul>
+    </div>
+)
+
+const AppComponent = () => (
+    <Header />
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppComponent />);
---
+++ code-files/React/4-first-react-project/notes.js
@@ -0,0 +1,38 @@
+
+
+/**
+ 
+Low Level Design of Component Heirarchy Layout :
+
+    App Component
+       -Header
+        --Logo
+        --Location
+        --cart
+        --account
+       
+       -Body
+        --Search
+            ---search button
+            ---search button
+            ---filter
+                ---price low to high
+                ---price high to low
+                ---ratings high to low
+                ---distance nearest to farthest
+                ---Cuisines offered
+                    -----cuisines list
+        
+       --Restruants      
+           ---Restrurant Card
+            ----Image
+            ----Rating
+            ----Distance
+            ----Average Price
+            ----Cuisines offered
+            
+       -Footer
+        --Contact
+        --Help
+        --About
+ */
