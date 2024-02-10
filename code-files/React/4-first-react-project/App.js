34a413ec59fd17b20aae15394f6c158bbf5a3ed7
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-11 00:17:00 +0530

    Formatting of LLD for better readinlitiy

--- code-files/React/4-first-react-project/App.js
+++ code-files/React/4-first-react-project/App.js
@@ -1,39 +1,36 @@
 import React from 'react' // importing 'React' module from node_modules/react
 import ReactDOM from 'react-dom' // importing 'ReactDOM' module from node_modules/react
 
-let heading = React.createElement('h1', {}, 'Hello React');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
-
 /**
- * Header
- *  Logo
- *  Location
- *  cart
- *  account
- * 
- * Body
- *  Search
- *      search button
- *      search button
- *      filter
- *          price low to high
- *          price high to low
- *          ratings high to low
- *          distance nearest to farthest
- *          Cuisines offered
- *              cuisines list
- *  
- *  Restrurant Card
- *      Image
- *      Rating
- *      Distance
- *      Average Price
- *      Cuisines offered
+ * App Component
+ *      -Header
+ *       --Logo
+ *       --Location
+ *       --cart
+ *       --account
  *      
- * Footer
- *  Contact
- *  Help
- *  About
- 
+ *      -Body
+ *       --Search
+ *           ---search button
+ *           ---search button
+ *           ---filter
+ *               ---price low to high
+ *               ---price high to low
+ *               ---ratings high to low
+ *               ---distance nearest to farthest
+ *               ---Cuisines offered
+ *                   -----cuisines list
+ *       
+ *      --Restruants      
+ *          ---Restrurant Card
+ *           ----Image
+ *           ----Rating
+ *           ----Distance
+ *           ----Average Price
+ *           ----Cuisines offered
+ *           
+ *      -Footer
+ *       --Contact
+ *       --Help
+ *       --About
  */
