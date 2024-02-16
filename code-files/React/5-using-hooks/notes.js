2b38303ba5c3eed0e103ebfef861b1362ab42243
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-17 01:23:37 +0530

    Added notes for ->     Good file and code management practices
        Types of Exports

--- code-files/React/5-using-hooks/notes.js
+++ code-files/React/5-using-hooks/notes.js
@@ -1,42 +1,37 @@
+/*
+    Good Practices :
+    
+    -> Always store the code files in a src folder
+    -> Separate folder should be created for all the components
+        -> Every component should have it's separate file
+    -> All the other common dependencies(not modules) should be stored inside a utilities/commons folder
+    -> All the hardcoded strings/values should be stored inside a separate constants file
+*/
 
 
-/**
- 
-Low Level Design of Component Heirarchy Layout :
+/*
+    Types of Exports :
 
-    App Component
-       -Header
-        --Logo
-        --Location
-        --cart
-        --account
-       
-       -Body
-        --Search
-            ---search box
-            ---search button
-            ---filter
-                ---price low to high
-                ---price high to low
-                ---ratings high to low
-                ---distance nearest to farthest
-                ---Cuisines offered
-                    -----cuisines list
-        
-       --Restruants      
-           ---Restrurant Card
-            ----Image
-            ----Rating
-            ----Delivery Time
-            ----Average Price
-            ----Cuisines offered
-            
-       -Footer
-        --Contact
-        --Help
-        --About
+    Default export ->
+        Syntax -> export default {object/variable name}
+
+        Functioning ->
+        When default export is used in a file, the argument mentioned inside the export statement is sent where the given module is imported.
+
+        Can be used once per file.
+
+    Named export ->
+        Syntax -> 
+            export const argument1 = value1;
+            export const argument2 = value2;
+
+        Functioning ->
+        This type of export is used when there are multiple entities that are needed to be exported depending on the use case.
+
+        When this type of file is imported, all the entities with export keyword are nested in an objectt and can be decomposed at the source of import call.
+
+        For example :
+
+        import {argument1} from 'file-path/file-name'
  */
 
-/**
-
-*/
