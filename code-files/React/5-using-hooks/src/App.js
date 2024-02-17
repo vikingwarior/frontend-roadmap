62c88c28ffa7c63767296153e2aeb084c4a3c722
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-18 02:47:32 +0530

    Added semicolons to import statemets

--- code-files/React/5-using-hooks/src/App.js
+++ code-files/React/5-using-hooks/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react' // importing 'React' module from node_modules/react
-import ReactDOM from "react-dom/client"  // importing 'ReactDOM' module from node_modules/react
+import React from 'react'; // importing 'React' module from node_modules/react
+import ReactDOM from "react-dom/client";  // importing 'ReactDOM' module from node_modules/react
 
 // Importing components
 import Header from './components/Header';
@@ -10,7 +10,7 @@ const AppComponent = () => (
         <Header />
         <Body />
     </div>
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppComponent />);
