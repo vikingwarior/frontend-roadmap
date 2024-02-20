612d4123017497e8908cb8d475eeb73ab1d55e4a
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-21 02:43:10 +0530

    Moved useEffect Hook to body component

--- code-files/React/6-react-app-ecosystem/src/components/Body.js
+++ code-files/React/6-react-app-ecosystem/src/components/Body.js
@@ -5,7 +5,8 @@
     To import a hook:
     import {hookName} from 'react'
  */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { API_URL } from '../utils/constants'
 import Search from './Search';
 import Restaurant from './Restaurant'
 
@@ -18,6 +19,21 @@ const Body = () => {
         let[varName, serVarName] = useState(initalVale);
      */
     let [restaurantsList, setRestaurantsList] = useState();
+
+    /*
+        useEffect() =>
+        React Hook that is used to run a function when the component is rendered or removed from the DOM.
+
+        It is trigger at the end of the render and removal states component lifcycle.
+
+        Initalizing useEffect hook:
+        useEffect(() =>{'callback function'}, [Dependency Array]);
+     */
+    useEffect(async () => {
+        const data = await fetch(API_URL);
+        const dataJSON = await data?.json();
+        setRestaurantsList(dataJSON.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }, []);
 
     console.log("component rendered");
     return (
