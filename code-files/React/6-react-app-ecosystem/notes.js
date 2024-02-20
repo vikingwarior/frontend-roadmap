157844850c82d92af9aa2527a505944c939e8090
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-20 23:38:13 +0530

    Notes monolith vs microservices

---
+++ code-files/React/6-react-app-ecosystem/notes.js
@@ -0,0 +1,14 @@
+/*
+    Monolith vs Microservices:
+
+    Monolith:
+    ----------
+    Entire code is written in one single project
+
+    Microservices:
+    ---------------
+    Every aspect of the application is defined in a separate project
+    follows separation of concerns and pricliple of single responsibility
+    
+
+*/
