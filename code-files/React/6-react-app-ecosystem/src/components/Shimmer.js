c938c009500a280fd3c49c4b975ebba8f6acc972
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-22 00:48:59 +0530

    Added more placeholders in shimmer

--- code-files/React/6-react-app-ecosystem/src/components/Shimmer.js
+++ code-files/React/6-react-app-ecosystem/src/components/Shimmer.js
@@ -3,48 +3,78 @@ const Shimmer = () => {
         <div className="shimmer container restaurants">
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
             <div className="card">
                 <div className="img"></div>
+                <div className="name"></div>
+                <div className="stats"></div>
             </div>
         </div>
     )
