9ed34a8102f64dbb180f33d50af2b64ac1bfea10
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-26 23:30:42 +0530

    Formatting changes

--- code-files/React/6-react-app-ecosystem/src/utils/constants.js
+++ code-files/React/6-react-app-ecosystem/src/utils/constants.js
@@ -1,6 +1,5 @@
 export const API_URL = {
     GET : 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
-
     POST: 'https://www.swiggy.com/dapi/restaurants/list/update'
 };
 
