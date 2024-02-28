54fff96ef658afa9d2f3ec81926e3eaa35ec29ca
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-29 00:37:51 +0530

    Added router for app and basic configuration

--- code-files/React/7-routes/src/App.js
+++ code-files/React/7-routes/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'; // importing 'React' module from node_modules/react
 import ReactDOM from "react-dom/client";  // importing 'ReactDOM' module from node_modules/react
+import { createBrowserRouter } from 'react-router-dom' // Importing Routing configurator for application
 
 // Importing components
 import Header from './components/Header';
@@ -13,6 +14,21 @@ const AppComponent = () => {
         </div>
     )
 };
+
+/**
+ *  To  create Routes, i.e. Paths to various pages of an application react-router-dom is used
+*/
+
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        component: <AppComponent />
+    },
+    {
+        path: '/about',
+        component: <About />
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppComponent />);
