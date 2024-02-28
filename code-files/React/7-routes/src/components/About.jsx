f0ae87b4c865013022f98c233b0cb0b5a94c8d70
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-02-29 01:40:36 +0530

    Added content to About component

---
+++ code-files/React/7-routes/src/components/About.jsx
@@ -0,0 +1,25 @@
+import Header from "./Header";
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <h1>About Us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        libero facilis, vel debitis amet hic sequi iure saepe eos voluptatibus!
+        Modi architecto quibusdam aut, voluptate dolore cum aliquid explicabo
+        officia exercitationem? Dolorem possimus aut, cumque explicabo id illo
+        quibusdam. Quos mollitia explicabo tenetur temporibus nihil dicta
+        possimus consectetur sed sapiente amet, nostrum quia pariatur
+        dignissimos doloremque id, totam, iste qui? Numquam quod dicta
+        exercitationem rem quae dolores accusantium odio non culpa impedit hic
+        iste quaerat, cupiditate dolor omnis iure fuga provident, voluptatibus
+        quis molestias. Officiis consequuntur, quo fugiat voluptatem a
+        accusamus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
