50c07e82a7f9f10523e9f49ec1ed1baa166107ec
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-03-01 22:55:36 +0530

    Added Error component

---
+++ code-files/React/7-routes/src/components/Error.jsx
@@ -0,0 +1,23 @@
+/*
+    useRouteError -> Hook provided by react-router-dom that fetches the error details in case of a route error.
+ */
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+    const { error, status } = useRouteError();
+    console.log();
+    return (
+        <div>
+            <h1>Oops!! Seems like you encountered an error</h1>
+            <h2>Error Message :</h2>
+            <h3>
+                Status :  {status}
+            </h3>
+            <h3>
+                Message : {error.message}
+            </h3>
+        </div>
+    );
+};
+
+export default Error;
