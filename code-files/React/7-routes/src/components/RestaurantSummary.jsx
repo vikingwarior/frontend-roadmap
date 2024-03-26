ca1e3a13b842cb6f307dd912d2c56a91f7abd7cd
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-03-26 20:59:10 +0530

    Added Component Outline

--- code-files/React/7-routes/src/components/RestaurantSummary.jsx
+++ code-files/React/7-routes/src/components/RestaurantSummary.jsx
@@ -1,10 +1,68 @@
+/**
+    Component Outline:
+    Heading Div
+        Restaurant Details Div
+            Breadcrumb
+            Div
+                Div
+                    Restaurant Name
+                    Cuisines
+                    Location, Avg Distance
+                Div
+                    Avg. Rating
+                    Total Ratings
+            Div
+                BikeRiderIcon
+                Distance From Location
+                |
+                Delivery Charge
+        Metrics Div
+            Basic Stats Div
+                Avg. Delivery Time
+                Cost for two
+            Discount Coupons Div
+                Discount Coupon Div
+                    {Discount Info}
+                    {Coupon Code} | {Coupon Condition}
+                Discount Coupon Div
+                Discount Coupon Div
+                ...     
+    Menu Div
+        Preferences Toggle Buttons
+        Top Picks Div
+            Top Picks
+            Top Picks Cards Div
+                Top Pick Card Div
+                    Cuisine Type Icon
+                    Item Name
+                    Item Summary
 
+                    Item Price
+                    Add Button Div
+                        {Add +}Button
+                        customisable
+                Top Pick Card Div
+                Top Pick Card Div
+                ...
+        Menu Categories Div
+            Menu Category Div
+                Category Name
+                Menu Item Div
+                    Menu Item Description Div
+                        Food Type Icon
+                        Item Name
+                        Item Price | Applicable Offer
+                    Menu Item Image Div
+                        Menu Item Image
+                        {Add +} Button
+    Browse Menu Button
+
+
+
+
+ */
 const RestaurantSummary = () => {
-    return (
-        <div>
-            Test
-        </div>
-    );
+  return <div>Test</div>;
 };
 
 export default RestaurantSummary;
