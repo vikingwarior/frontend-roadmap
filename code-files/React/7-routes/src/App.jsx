7b2c39022b30808970fecc47a6a80e031c7ab63c
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-04-05 23:27:28 +0530

    Formatting change

--- code-files/React/7-routes/src/App.jsx
+++ code-files/React/7-routes/src/App.jsx
@@ -15,7 +15,7 @@ const AppComponent = () => {
       <Header />
       {/*
           <Outlet> -> used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. If the parent route matched exactly, it will render a child index route or nothing if there is no index route.
-       */}
+      */}
       <Outlet />
     </div>
   );
