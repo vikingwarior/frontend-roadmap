3bcb28f384b86d7e8d82e2f2fade9c190bc213d1
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-04-08 21:39:04 +0530

    Hardcoded Breadcrumb

--- code-files/React/7-routes/src/components/BreadCrumb.jsx
+++ code-files/React/7-routes/src/components/BreadCrumb.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BreadCrumb = () => {
-  return <div className="breadCrumb">Breadcrumb</div>;
+  return (
+  <div className="breadCrumb">
+    <span><Link to='localhost:1234'>Home</Link></span>  
+    <span>/</span>  
+    <span><Link to='localhost:1234'>Pune</Link></span>  
+    <span>/</span>  
+    <span>McDonald's</span>  
+  </div>
+  );
 };
 
 export default BreadCrumb;
