4f5206a4cf559b481f6419b8c4e9eb1c3dae14db
Harsh Paradkar <hparadkar@ptc.com>
2024-04-23 02:06:43 +0530

    new component

---
+++ code-files/src/components/MenuCategory.jsx
@@ -0,0 +1,14 @@
+const MenuCategory = ({title, categoryItems}) => {
+  return (
+    <div className="menuCategory">
+      <h3>{title}</h3>
+      {
+        categoryItems.map((item)=>{
+
+        })
+      }
+    </div>
+  );
+};
+
+export default MenuCategory;
