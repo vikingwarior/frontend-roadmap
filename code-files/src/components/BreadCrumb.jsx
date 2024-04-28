af8c04db99af818983b535428875b3cda504f829
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-04-29 01:32:41 +0530

    new breadcrumb path

--- code-files/src/components/BreadCrumb.jsx
+++ code-files/src/components/BreadCrumb.jsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 
 const BreadCrumb = () => {
   return (
-  <div className="breadCrumb">
-    <span><Link to='localhost:1234'>Home</Link></span>  
-    <span>/</span>  
-    <span><Link to='localhost:1234'>Pune</Link></span>  
-    <span>/</span>  
-    <span>McDonald's</span>  
-  </div>
+    <div className="breadCrumb">
+      <span>
+        <Link to="../../">Home</Link>
+      </span>
+      <span>/</span>
+      <span>
+        <Link to="../../">Pune</Link>
+      </span>
+      <span>/</span>
+      <span>McDonald's</span>
+    </div>
   );
 };
 
