9f6fb5cb9c127309ca1fe646e884446483ddfed5
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-06-15 00:34:55 +0530

    Adds a button to each menu item

--- code-files/src/components/MenuItem.jsx
+++ code-files/src/components/MenuItem.jsx
@@ -1,17 +1,20 @@
-const MenuItem = ({itemData}) => {
-    const {name, price, description, ratings } = itemData;
-    const rating = ratings.aggregatedRating.rating || 0;
-    const ratingCount = ratings.aggregatedRating.ratingCountV2 || 0;
+const MenuItem = ({ itemData }) => {
+  const { name, price, description, ratings } = itemData;
+  const rating = ratings.aggregatedRating.rating || 0;
+  const ratingCount = ratings.aggregatedRating.ratingCountV2 || 0;
 
-    return (
-        <div className="">
-            <h3>{name}</h3>
-            <h4>₹{price/100}</h4>
-            <h5>{rating}({ratingCount})</h5>
-            <p>{description}</p>
-            <hr />
-        </div>
-    );
+  return (
+    <div className="">
+      <h3>{name}</h3>
+      <h4>₹{price / 100}</h4>
+      <h5>
+        {rating}({ratingCount})
+      </h5>
+      <p>{description}</p>
+      <button className="auth button p-2 my-3">Add +</button>
+      <hr />
+    </div>
+  );
 };
 
 export default MenuItem;
