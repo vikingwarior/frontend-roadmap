d7c4e46e3c44c5918bcedf5243d065acd8dd6580
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-06-17 02:18:18 +0530

    Integrates Menu Component with cart

--- code-files/src/components/MenuItem.jsx
+++ code-files/src/components/MenuItem.jsx
@@ -1,7 +1,12 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../utils/redux/cartSlice";
+
 const MenuItem = ({ itemData }) => {
   const { name, price, description, ratings } = itemData;
   const rating = ratings.aggregatedRating.rating || 0;
   const ratingCount = ratings.aggregatedRating.ratingCountV2 || 0;
+
+  const dispatch = useDispatch();
 
   return (
     <div className="">
@@ -11,7 +16,15 @@ const MenuItem = ({ itemData }) => {
         {rating}({ratingCount})
       </h5>
       <p>{description}</p>
-      <button className="auth button p-2 my-3">Add +</button>
+      <button
+        className="auth button p-2 my-3"
+        onClick={() => {
+          console.log(itemData);
+          dispatch(addItem(itemData));
+        }}
+      >
+        Add +
+      </button>
       <hr />
     </div>
   );
