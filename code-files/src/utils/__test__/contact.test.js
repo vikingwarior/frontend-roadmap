576824b35ce997083b456a0cde44356aff530ed1
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-06-24 23:58:07 +0530

    Adds base test cases

--- code-files/src/utils/__test__/contact.test.js
+++ code-files/src/utils/__test__/contact.test.js
@@ -1,3 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import Contact from "../../components/Contact"
+
 test('Tests Load for contact page', () => {
+
+    render(<Contact />);
+
+    const contactNode = screen.findByRole("button");
+    expect(contactNode).toBeInTheDocument();
   
 })
