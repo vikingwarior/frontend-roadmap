3f2203c8cb358c961232841933cdd27dc206f115
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-02 02:22:13 +0530

    Adds comments

--- code-files/src/utils/__test__/contact.test.js
+++ code-files/src/utils/__test__/contact.test.js
@@ -5,28 +5,37 @@ import { render, screen } from "@testing-library/react"
 import Contact from "../../components/Contact"
 import '@testing-library/jest-dom';
 
-test('Tests Load for contact page', () => {
+/**
+ * describe block is used to group test cases.
+ */
+describe('test cases', () => {
+    test('Tests Load for contact page', () => {
 
-    render(<Contact />);
-    const contactUsElem = screen.getByTestId("contact-us");
+        render(<Contact />);
+        const contactUsElem = screen.getByTestId("contact-us");
+        
+        expect(contactUsElem).toBeInTheDocument();
+    });
     
-    expect(contactUsElem).toBeInTheDocument();
+    test('Checks if all the textBoxes are loaded or not', () => {
+    
+        render(<Contact />);
+    
+        const textBoxNodes = screen.getAllByRole("textbox");
+        
+        expect(textBoxNodes.length).toBe(4);
+    });
+    
+    /**
+     * 'it' is an alternative name for 'test' method
+     */
+    it('Checks if the submit button is loaded', () => {
+    
+        render(<Contact />);
+    
+        const textBoxNodes = screen.getAllByText("Let's talk");
+        
+        expect(textBoxNodes.length).toBe(1);
+    });
 });
 
-test('Checks if all the textBoxes are loaded or not', () => {
-
-    render(<Contact />);
-
-    const textBoxNodes = screen.getAllByRole("textbox");
-    
-    expect(textBoxNodes.length).toBe(4);
-});
-
-test('Checks if the submit button is loaded', () => {
-
-    render(<Contact />);
-
-    const textBoxNodes = screen.getAllByText("Let's talk");
-    
-    expect(textBoxNodes.length).toBe(1);
-});
