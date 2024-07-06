afbe4dc2d0f87d6e2d39a793c9277a011e3cf120
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-07 04:08:22 +0530

    Adds integration tests for Search functionality

--- code-files/src/utils/__test__/Search.test.js
+++ code-files/src/utils/__test__/Search.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import "@testing-library/jest-dom";
 
@@ -28,5 +28,32 @@ describe("Search Integration tests", () => {
         </BrowserRouter>
       );
     });
+
+    /**
+        As a second argument in these getter functions(getByText, getByRole, etc.), an object containing details of element(name, type, id, etc) to be fetched can be passed as second argument. 
+     */
+    const searchTextBox = screen.getByTestId("inp-text-box");
+    const searchBtn = screen.getByRole("button", {name: "Search"});
+
+
+    // Assertion to check if btn and box is loaded or not
+    expect(searchTextBox).toBeInTheDocument();
+    expect(searchBtn).toBeInTheDocument();
+
+    //Modify search box
+
+    // Since jest-dom doesn't have browser events we have to mock them
+    const browserEventMock = {target : {value : "Burger"}};
+
+    // Assertion to check the rescards are loaded (total 20 cards are loaded by default as per mock data)
+    const resCards = screen.getAllByTestId("res-card")
+    expect(resCards.length).toBe(20);
+
+    fireEvent.change(searchTextBox, browserEventMock)
+    fireEvent.click(searchBtn);
+
+    //Assertion to validate entries for search query for word "Burger"(Total 4 entries have word burger in them)
+    const newResCards = screen.getAllByTestId("res-card")
+    expect(newResCards.length).toBe(4);
   });
 });
